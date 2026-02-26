11005f6d989e950d09b8c6e3d097cb8dafdbb55f
Andrew-McCrow <andrewmccrow@gmail.com>
2026-02-26 16:04:21 +0000

    added prisma (up to quickstart part 5), passport auth and other dependencies

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-// install dependencies
+//
+const path = require("node:path");
+const { Pool } = require("pg");
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const pool = new Pool({
+  // add your configuration
+});
+
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-// routes
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
-// start the server
-const PORT = 3000;
-app.listen(PORT, (error) => {
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, (error) => {
   if (error) {
     throw error;
   }
-  console.log(`My first Express app - listening on port ${PORT}!`);
+  console.log("app listening on port 3000!");
 });
